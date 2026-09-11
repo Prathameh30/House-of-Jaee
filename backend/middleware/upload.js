b32444bd0c849313b0d710eff38fc4ee797e9c59
Prathameh30 <prathameshpatil8946@gmail.com>
2026-09-11 10:54:17 +0530

    Increase image upload size limit

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -15,7 +15,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5 MB
+    fileSize: 10 * 1024 * 1024, // 5 MB
   },
   fileFilter,
 });
